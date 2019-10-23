d30a35e2f462e5af87ac841516665579eecf4959
Robert Wafula <rbwafula@gmail.com>
2019-10-23 19:54:52 +0300

    version 0.19.10.19.8

--- wp-content/themes/cialca-child/assets/js/barchart.js
+++ wp-content/themes/cialca-child/assets/js/barchart.js
@@ -44,13 +44,120 @@ jQuery(document).ready(function($){
         });
 
         Highcharts.chart('impactchart1', {
-        colors: ['#4472C4','transparent'],
+            colors: ['#4472C4','transparent'],
             chart: {
                 backgroundColor: 'transparent',
                 plotBackgroundColor: 'transparent',
                 plotBorderWidth: null,
                 plotShadow: false,
                 type: 'pie'
+            },
+            navigation: {
+                buttonOptions: {
+                    enabled: false
+                }
+            },
+            credits: false,
+            title: {
+                text: 'Test Chart'
+            },
+            subtitle: {
+                text: 'Alex kindly modify to your use'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: false,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.percentage:.1f} %',
+                        distance: -100,
+                           color:'white'
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: [{
+                    name: 'Chrome',
+                    y: 70,
+                    sliced: false,
+                    selected: true
+                }, {
+                    name: '',
+                    y: 30
+                }]
+            }]
+        });
+
+        Highcharts.chart('impactchart2', {
+            colors: ['#4472C4','transparent'],
+            chart: {
+                backgroundColor: 'transparent',
+                plotBackgroundColor: 'transparent',
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            navigation: {
+                buttonOptions: {
+                    enabled: false
+                }
+            },
+            credits: false,
+            title: {
+                text: 'Test Chart'
+            },
+            subtitle: {
+                text: 'Alex kindly modify to your use'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: false,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.percentage:.1f} %',
+                        distance: -100,
+                           color:'white'
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: [{
+                    name: 'Chrome',
+                    y: 70,
+                    sliced: false,
+                    selected: true
+                }, {
+                    name: '',
+                    y: 30
+                }]
+            }]
+        });
+
+        Highcharts.chart('impactchart3', {
+            colors: ['#4472C4','transparent'],
+            chart: {
+                backgroundColor: 'transparent',
+                plotBackgroundColor: 'transparent',
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            navigation: {
+                buttonOptions: {
+                    enabled: false
+                }
             },
             credits: false,
             title: {
